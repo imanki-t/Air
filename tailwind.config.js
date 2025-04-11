454aa671c2e23fc7dbaa1f373aad323b609d702b
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-04-11 22:51:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        cat: "url('/cat-bg.jpg')"
+        cat: "url('./public/cat-bg.jpg')" // Updated path to be more specific
       },
       fontFamily: {
         vintage: ['"Press Start 2P"', 'cursive'],

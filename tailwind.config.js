534a7e6738c1cded2624ddbc34a38a4a586fd78a
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-04-12 01:12:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,16 @@ export default {
       },
       animation: {
         retro: 'retro-fade 0.5s ease-out',
+        'fade-out': 'fadeOut 0.5s ease-in forwards', // added fade-out animation
       },
       keyframes: {
         'retro-fade': {
           '0%': { transform: 'scale(0.9)', opacity: 0 },
           '100%': { transform: 'scale(1)', opacity: 1 },
+        },
+        'fadeOut': {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0, visibility: 'hidden' },
         }
       }
     }

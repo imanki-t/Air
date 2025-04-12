1d31ed5fd380a4623b681d69636b157c4780189b
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-04-12 11:37:19 +0530

    Update FileItem.jsx

--- src/components/FileItem.jsx
+++ src/components/FileItem.jsx
@@ -93,7 +93,7 @@ const FileItem = ({ file, refresh }) => {
       {/* Share Modal */}
       {showShare && (
         <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50 px-4">
-          <div className="bg-yellow-600 border-4 border-red-600 p-6 pt-8 pr-8 rounded-xl max-w-sm w-full text-center relative shadow-vintage">
+          <div className="bg-yellow-100 border-4 border-red-600 p-6 pt-8 pr-8 rounded-xl max-w-sm w-full text-center relative shadow-vintage">
             
             <button
               onClick={() => setShowShare(false)}

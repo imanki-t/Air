aefefb941698a93e9ed90d15c9b858362ec65fe5
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-04-12 10:21:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Specifies the output directory for the build
   },
   define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL || '')
-  }
-})
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL || ''),
+  },
+  optimizeDeps: {
+    include: ['qrcode.react'], // Ensures qrcode.react is included in the dependency optimization
+  },
+});

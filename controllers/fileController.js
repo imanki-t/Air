8bfc3a5cb135b663eaa95d28ddce40ccbe985227
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-04-29 19:58:29 +0530

    Update fileController.js

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,12 +1,13 @@
 // controllers/fileController.js
-const fileService = require('../services/fileService');
+const fileService = require('../services/fileService'); // 
 
-module.exports = {
-  uploadFile: fileService.uploadFile,
-  getFiles: fileService.getFiles,
-  deleteFile: fileService.deleteFile,
-  downloadFile: fileService.downloadFile,
-  cleanupIncompleteUpload: fileService.cleanupIncompleteUpload,
-  generateShareLink: fileService.generateShareLink,
-  accessSharedFile: fileService.accessSharedFile,
+module.exports = { // 
+  uploadFile: fileService.uploadFile, // 
+  getFiles: fileService.getFiles, // 
+  deleteFile: fileService.deleteFile, // 
+  downloadFile: fileService.downloadFile, // 
+  cleanupIncompleteUpload: fileService.cleanupIncompleteUpload, // 
+  generateShareLink: fileService.generateShareLink, // 
+  accessSharedFile: fileService.accessSharedFile, // 
+  uploadAndShareZip: fileService.uploadAndShareZip, // +++ Add the new controller mapping +++
 };

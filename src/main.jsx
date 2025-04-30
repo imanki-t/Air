66757d21b623ff3a50dd22a4d72580d64597c989
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-04-30 23:06:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { HashRouter } from 'react-router-dom'
+ import React from 'react'
+ import ReactDOM from 'react-dom/client'
+ import App from './App'
+ import './index.css'
+ // Import BrowserRouter instead of HashRouter
+ import { BrowserRouter } from 'react-router-dom' // Changed 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-)
+ ReactDOM.createRoot(document.getElementById('root')).render(
+   <React.StrictMode>
+     {/* Use BrowserRouter */}
+     <BrowserRouter> {/* Changed */}
+       <App />
+     </BrowserRouter>
+   </React.StrictMode>
+ ) 

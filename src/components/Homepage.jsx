fa5a82d125439b17252e477254c33a1ac891313d
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-02 19:11:10 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -211,8 +211,8 @@ const Homepage = ({ isLoggedIn }) => {
         <svg className="absolute w-full min-w-[1000px] h-24 transform rotate-180" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-             className={`${darkMode ?
-'fill-gray-950' : 'fill-white'}`} {/* Changed fill color to match dark mode background */}
+            className={`${darkMode ?
+'fill-gray-950' : 'fill-white'}`} /* Changed fill color to match dark mode background */
           ></path>
         </svg>
       </div>
@@ -299,8 +299,8 @@ const Homepage = ({ isLoggedIn }) => {
         <svg className="absolute w-full min-w-[1000px] h-24 transform rotate-180" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-             className={`${darkMode ?
-'fill-gray-950' : 'fill-white'}`} {/* Changed fill color to match dark mode background */}
+            className={`${darkMode ?
+'fill-gray-950' : 'fill-white'}`} /* Changed fill color to match dark mode background */
           ></path>
         </svg>
       </div>
@@ -381,4 +381,4 @@ const Homepage = ({ isLoggedIn }) => {
 };
 
 export default Homepage;
-                
+              

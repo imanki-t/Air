5da578134765f2282b023baea114d2a2639ced00
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-03 12:53:01 +0530

    Update SignupPage.jsx

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -1,6 +1,6 @@
 // components/SignupPage.jsx
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const SignupPage = ({ darkMode }) => {

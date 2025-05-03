e0898afb6a88a83879ce4dbdffccfecc14468266
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-03 20:36:59 +0530

    Update UploadForm.jsx

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -627,7 +627,7 @@ const UploadForm = ({ refresh, darkMode }) => {
               </p>
             </div>
             {/* Adjusted button container: removed gap-4 from flex, added mr-4 to first button */}
-            <div className="flex justify-center space-x-4 mt-4">
+            <div className="flex justify-center space-x-2 mt-4">
               <button
                 type="button"
                 onClick={handleResumeUpload}

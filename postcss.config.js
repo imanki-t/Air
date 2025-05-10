c4a5897e6c1bc9cbe5f850683a87c72d92f29201
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-10 15:33:01 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: [
-    'tailwindcss', // this will use the Tailwind CSS plugin
-    'autoprefixer', // this will use the Autoprefixer plugin
-  ],
-};
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}

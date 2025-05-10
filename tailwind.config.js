1b128c576cbfc4fcb1416aa08ca2ab208e77ada6
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-10 13:28:48 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // Use 'class' to control dark mode manually
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'], // in case you want to refer to it as 'inter'
+        inter: ['Inter', 'sans-serif'], 
       },
       colors: {
         'timeless-dark': '#0f172a',

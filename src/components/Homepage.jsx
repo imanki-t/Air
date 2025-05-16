1aabdcd6d26971780af984ce96188a9318a6c378
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-16 22:52:51 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -154,7 +154,7 @@ backgroundSize: '30px 30px',
       <nav className={`relative z-10 px-6 py-6 mx-auto max-w-7xl ${darkMode ? 'bg-gray-950' : 'bg-white'}`}>
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <img src="airstream.png" alt="Airstream" className="w-20 h-20 rounded-lg" />
+            <img src="air.png" alt="Airstream" className="w-20 h-20 rounded-lg" />
 <span className="text-4xl font-extrabold tracking-tight">AIRSTREAM</span>
           </div>
 

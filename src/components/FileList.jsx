07dfc7a6f3ae0317baec06b4669e09e87035661a
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-17 15:27:37 +0530

    Update FileList.jsx

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -592,10 +592,8 @@ const zipFilename = `AIRSTREAM${timestamp}${randomCombo}.zip`;
              title="Sort & Filter"
            >
              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2}>
-  {/* Up Arrow */}
-  <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
-  {/* Down Arrow */}
-  <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l4-4m0 0l4 4M7 4v12" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21 16l-4 4m0 0l-4-4m4 4V8" />
 </svg>
            </button>
            {/* Dropdown Panel */}

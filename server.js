9448a5abbd812ec958d31126edc2d77e3f010ad9
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-17 14:37:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const fileRoutes = require('./routes/fileRoutes');
+const http = require('http');
+const { Server } = require('socket.io');
 
 dotenv.config();
 const app = express();
-
-// Connect to MongoDB
 connectDB();
 
 // Middleware
@@ -18,12 +18,36 @@ app.use(express.json());
 // Routes
 app.use('/api/files', fileRoutes);
 
-// Root route
+// Root
 app.get('/', (req, res) => {
   res.send('Storage API');
 });
 
+// Create HTTP server for WebSocket
+const server = http.createServer(app);
+
+// Socket.IO server
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('fileUploaded', () => {
+    console.log('Received fileUploaded, broadcasting refreshFileList...');
+    socket.broadcast.emit('refreshFileList');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

0d242644dd9890d138c1700907bf4e62b355f653
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-22 13:59:36 +0530

    Update FileList.jsx

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -801,7 +801,7 @@ onClick={() => setShowSortOptions(prev => !prev)}
         )}>
           {/* Render File Items */}
           {paginatedFiles.map(file => (
-            <div className="relative overflow-visible" key={file._id}>
+            
             <FileItem
               key={file._id}
               file={file}
@@ -813,7 +813,7 @@ onClick={() => setShowSortOptions(prev => !prev)}
               selectionMode={selectionMode}
               refresh={refresh}
             />
-          </div>    
+            
           ))}
         </div>
       ) : (

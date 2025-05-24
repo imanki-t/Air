1063b03470ba0997acade70e9adef1edd6f193de
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-24 09:11:04 +0530

    Update FileItem.jsx

--- src/components/FileItem.jsx
+++ src/components/FileItem.jsx
@@ -222,7 +222,7 @@ containerBaseClasses += ' h-32 mb-2 rounded-t-xl'; // Larger size for grid view
 if (type === 'image') {
 return (
   <div className={containerBaseClasses}>
-    <img src={thumbnailUrl} alt={`Preview of ${file.filename}`} className={imageVideoPreviewClasses} loading="lazy" />
+    <img src={previewUrl} alt={`Preview of ${file.filename}`} className={imageVideoPreviewClasses} loading="lazy" />
     <div className="absolute inset-0 bg-gradient-to-t from-black/30 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
   </div>
 );
@@ -231,7 +231,7 @@ return (
 if (type === 'video') {
 return (
   <div className={containerBaseClasses}>
-    <video src={`${thumbnailUrl}#t=0.5`} preload="metadata" className={`${imageVideoPreviewClasses} bg-black`} />
+    <video src={`${previewUrl}#t=0.5`} preload="metadata" className={`${imageVideoPreviewClasses} bg-black`} />
     <div className="absolute inset-0 flex items-center justify-center bg-black/20 group-hover:bg-black/40 transition-all duration-300">
        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white/70 drop-shadow-lg" fill="currentColor" viewBox="0 0 20 20">
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8.118v3.764a1 1 0 001.555.832l3.197-1.882a1 1 0 000-1.664l-3.197-1.882z" clipRule="evenodd" />
